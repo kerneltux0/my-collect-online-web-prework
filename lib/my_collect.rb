81457093b282ece061bd8af4dcfc7c0d8c8efb60
Ryan Sise <kerneltux0@gmail.com>
2019-02-12 20:25:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
+names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
 
+def my_collect(array)
+  if block_given?
+    i = 0
+    while i < array.size
+      yield(array[i])
+      i += 1
+    end
+    array
+  else
+    "Block not provided"
+  end
+end
+
+my_collect(names) do |name|
+  name.split(" ").first
+end
+
+my_collect(collection) do |lang|
+  lang.upcase
+end
