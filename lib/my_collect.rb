1c6b6fde66a32849a15091aaf81686d190e9dfb0
Ryan Sise <kerneltux0@gmail.com>
2019-02-12 20:25:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,12 +4,14 @@ collection = ['ruby', 'javascript', 'python', 'objective-c']
 
 def my_collect(array)
   if block_given?
+    new_array = []
     i = 0
-    while i < array.size
+    while i < array.length
       yield(array[i])
+      new_array << array
       i += 1
     end
-    array
+    new_array
   else
     "Block not provided"
   end
